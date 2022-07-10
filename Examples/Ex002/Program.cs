be20fdaa5acc937730c5f5b190286128491efb7d
Sergey Evseev <mresv@ya.ru>
2022-07-10 23:56:03 +0300

    Writeline modifed

--- Examples/Ex002/Program.cs
+++ Examples/Ex002/Program.cs
@@ -1,4 +1,4 @@
-﻿Console.WriteLine("Ведите Ваше имя");
+﻿Console.Write("Ведите Ваше имя");
 string username = Console.ReadLine();
-Console.WriteLine("Привет,");
-Console.WriteLine(username);
+Console.Write("Привет,");
+Console.Write(username);
