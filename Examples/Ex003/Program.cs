b0c2dc5bdb1d6cfd70281299ec9d8d05e17969c3
Sergey <mresv@ya.ru>
2022-07-14 16:42:12 +0300

    Exp003 modified

--- Examples/Ex003/Program.cs
+++ Examples/Ex003/Program.cs
@@ -1,4 +1,6 @@
-﻿int numA = 5;
-int numB = 7;
+﻿int numA = new random().next(1;10); //1..9
+Console.WriteLine(numA);
+int numB = new random().next(1;10);
+Console.WriteLine(numB);
 Console.Write("Сумма чисел 1 и 2 равна ");
 Console.Write(numA + numB);
