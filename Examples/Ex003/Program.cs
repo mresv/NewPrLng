46741f7c942a7f993e324fb11e3b7bbc0ff51077
Sergey <mresv@ya.ru>
2022-07-24 20:21:30 +0300

    Сбой после сохранения

--- Examples/Ex003/Program.cs
+++ Examples/Ex003/Program.cs
@@ -2,5 +2,6 @@
 Console.WriteLine(numA);
 int numB = new random().next(1;10);
 Console.WriteLine(numB);
+int res = numA + numB
 Console.Write("Сумма чисел 1 и 2 равна ");
-Console.Write(numA + numB);
+Console.Write(res);
